e616106c80bf7d346ce8ad1f4f3860a9fa9992a7
dreamcoin1998 <47708051+dreamcoin1998@users.noreply.github.com>
2022-07-11 00:38:38 +0800

    Update quick-test.sh

--- build/quick-test.sh
+++ build/quick-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
+sourcepath=$(cd `dirname $0`/../; pwd)
 . $basepath/env.sh
-cd $basepath
+cd $sourcepath
 
 bash build/build.sh -p 1 -o image
 docker run -e mode=test -p 13000:3000 -p 27017:27017 -v $sourcepath:/tapdata-source/ -i --name=$dev_container_name `cat image/tag` bash
