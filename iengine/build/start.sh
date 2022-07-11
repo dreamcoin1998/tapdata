fc3294c5eb6891b271322523913b6bc5909b484d
dreamcoin1998 <47708051+dreamcoin1998@users.noreply.github.com>
2022-07-11 15:45:26 +0800

    Update start.sh

--- iengine/build/start.sh
+++ iengine/build/start.sh
@@ -5,5 +5,6 @@ export app_type=DAAS
 export backend_url=http://127.0.0.1:3000/api/
 export TAPDATA_MONGO_URI='mongodb://127.0.0.1:27017/tapdata?authSource=admin'
 
+basepath=$(cd `dirname $0`; pwd)
 
-nohup java -jar lib/$sbin_file &> logs/$sbin_file.log &
+nohup java -jar lib/$sbin_file &> $basepath/../logs/$sbin_file.log &
