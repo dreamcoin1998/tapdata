2bde27d90ab5c6990c80d0325a7e74e558759a2c
dreamcoin1998 <47708051+dreamcoin1998@users.noreply.github.com>
2022-07-11 10:50:30 +0800

    Update quick-test.sh

--- build/quick-test.sh
+++ build/quick-test.sh
@@ -10,75 +10,42 @@ basepath=$(cd `dirname $0`; pwd)
 sourcepath=$(cd `dirname $0`/../; pwd)
 cd $basepath
 
-wait_do_something() {
-    array=$1
-    tmp=()
-    wait_time=$2
-    for i in $(seq 1 $wait_time); do
-
-        if [[ ${#array[@]} -eq 0 ]]; then
-            break
-        fi
-
-        for id in "${!array[@]}";
-        do
-            element=$array[$id]
-            ! test -f /var/run/$element && test -f /tmp/$element.log
-            if [[ $? -eq 0 ]]; then
-                info "this is $element pull log"
-                cat /var/log/$element.log
-                continue
-            else
-                tmp[$id]=$element
-            fi
-        done
-
-        if [[ ${#tmp[@]} -eq 0 ]]; then
-            break
-        else
-            tmp=()
-        fi
-
-        sleep 1
-    done
-}
-
-echo "start pull images..."
-
-touch /tmp/$docker_build.log
-touch /tmp/$docker_runtime.log
-touch /tmp/$docker_all_in_one.log
-
-sudo nohup bash ./pre.sh -m $docker_build -t "image" &> /tmp/$docker_build.log &
-sudo nohup bash ./pre.sh -m $docker_runtime -t "image" &> /tmp/$docker_runtime.log &
-sudo nohup bash ./pre.sh -m $docker_all_in_one -t "image" &> /tmp/$docker_all_in_one.log &
-
-image_list=( $docker_build $docker_runtime $docker_all_in_one )
-
-wait_do_something $image_list 300
-
-echo "stop pull images..."
-
 if [[ $force -eq 1 ]]; then
     docker rm -f $dev_container_name
     docker rmi -f `cat image/tag`
 fi
+
+nohup bash ./pre.sh -t image -m $docker_all_in_one &> $docker_all_in_one.log &
+nohup bash ./pre.sh -t image -m $docker_build &> $docker_build.log &
+nohup bash ./pre.sh -t image -m $docker_runtime &> $docker_runtime.log &
+
+sleep 1
+
+for i in $(seq 1 300); do
+    if [[ -f $docker_all_in_one.log && -f $docker_build.log && -f $docker_runtime.log && ! -f $docker_all_in_one.run && ! -f $docker_build.run && ! -f $docker_runtime.run ]]; then
+      info "this is $docker_all_in_one.log"
+      cat $docker_all_in_one.log
+      info "this is $docker_build.log"
+      cat $docker_build.log
+      info "this is $docker_runtime.log"
+      cat $docker_runtime.log
+      break
+    else
+      info "pulling images"
+    fi
+    sleep 1
+done
 
 docker ps|grep $dev_container_name &> /dev/null
 if [[ $? -ne 0 ]]; then
     tag=`cat image/tag`
     x=`docker images $tag|wc -l`
     if [[ $x -eq 1 ]]; then
-        sudo nohup bash ./pre.sh -m iengine -t "compire" &> /tmp/iengine.log &
-        sudo nohup bash ./pre.sh -m manager -t "compire" &> /tmp/manager.log &
-        sudo nohup bash ./pre.sh -m plugin-kit -t "compire" &> /tmp/plugin-kit.log &
-        sudo nohup bash ./pre.sh -m connectors -t "compire" &> /tmp/connectors.log &
-
-        module_list=("iengine" "manager" "plugin-kit" "connectors")
-
-        wait_do_something $module_list 300
-
         cd ../
+        bash build/build.sh -c iengine
+        bash build/build.sh -c manager
+        bash build/build.sh -c plugin-kit
+        bash build/build.sh -c connectors
         bash build/build.sh -p 1 -o image
     fi
     cd $basepath
