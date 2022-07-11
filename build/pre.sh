702225babf1250a96b30b2fe527cca0a6a0716e7
dreamcoin1998 <47708051+dreamcoin1998@users.noreply.github.com>
2022-07-11 10:50:05 +0800

    Update pre.sh

--- build/pre.sh
+++ build/pre.sh
@@ -4,52 +4,60 @@ sourcepath=$(cd `dirname $0`/../; pwd)
 . $basepath/log.sh
 . $basepath/env.sh
 
-cd $basepath
+cd $sourcepath
 
-type="image"
 module=""
+type="image"
 
 while getopts 't:m:' opt; do
 	case "$opt" in
 	't')
 		type="$OPTARG"
 		;;
-    'm')
-        module="$OPTARG"
-        ;;
+	'm')
+		module="$OPTARG"
+		;;
 	esac
 done
 
-pull_image() {
-    docker images | grep "$docker_registry$module"
-    if [[ $? -ne 1 ]]; then
-        docker pull $docker_registry$module
-    fi
+create_tag() {
+  echo "create $module run tag $module.run"
+  touch $module.run
 }
 
-create_file() {
-    sudo touch /var/run/$module
+delete_tag() {
+  echo "delete $module run tag $module.run"
+  touch $module.run
 }
 
-delete_file() {
-    sudo rm /var/run/$module
+run_compile() {
+  cd $basepath
+  echo "run module compile, module is $module"
+  chmod u+x build.sh
+  bash build.sh -c $module
 }
 
-compire() {
-    cd ../
-    bash build/build.sh -c $module
+run_docker_pull() {
+  cd $basepath
+  echo "run docker pull $module"
+  chmod u+x build.sh
+  docker images | grep $module
+  if [[ $? -ne 0 ]]; then
+    echo "docker image $module not found, pulling soon"
+    docker pull $docker_registry$module
+  else
+    echo "docker image $module is exists"
+  fi
 }
 
-create_file
+create_tag
 
 if [[ $type == "image" ]]; then
-    pull_image
+  run_docker_pull
 fi
 
-if [[ $type == "compire" ]]; then
-    compire
+if [[ $type == "compile" ]]; then
+  run_compile
 fi
 
-delete_file
-
-
+delete_tag
