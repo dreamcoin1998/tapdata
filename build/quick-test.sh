1856d1c03a4ada9487626fea379290e8d2ad7bac
dreamcoin1998 <47708051+dreamcoin1998@users.noreply.github.com>
2022-07-11 17:45:55 +0800

    Update quick-test.sh

--- build/quick-test.sh
+++ build/quick-test.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
-force=$1
-if [[ "x"$force == "x-f" ]]; then
-    force=1
-fi
+sourcepath=$(cd `dirname $0`/../; pwd)
 
 . $basepath/env.sh
-basepath=$(cd `dirname $0`; pwd)
-sourcepath=$(cd `dirname $0`/../; pwd)
+
 cd $basepath
-
-if [[ $TEST_DATABASE ]]; then
-    echo "存在"
-else
-    echo "不存在"
-fi
-
-echo "start base64 decode"
-echo $TEST_DATABASE | base64 -di > $sourcepath/tapshell/test/.env
-echo "end base64 decode"
-ls -al $sourcepath/tapshell/test/
-
-if [[ $force -eq 1 ]]; then
-    docker rm -f $dev_container_name
-    docker rmi -f `cat image/tag`
-fi
 
 docker ps|grep $dev_container_name &> /dev/null
 if [[ $? -ne 0 ]]; then
@@ -40,7 +20,5 @@ if [[ $? -ne 0 ]]; then
     fi
     cd $basepath
     docker run -e mode=test -p 13000:3000 -p 27017:27017 -v $sourcepath:/tapdata-source/ -i --name=$dev_container_name `cat image/tag` bash
-    if [[ $? -ne 0 ]]; then
-        exit 127
-    fi
+    exit $?
 fi
